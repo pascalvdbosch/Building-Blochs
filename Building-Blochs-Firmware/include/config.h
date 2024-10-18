2d4eaaaff131e2456befb29cda2bf9a2e50dbd67
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2024-10-18 17:00:53 +0200

    config params

--- Building-Blochs-Firmware/include/config.h
+++ Building-Blochs-Firmware/include/config.h
@@ -1,24 +1,14 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define VERSIONSTR "20230412"
+#define VERSIONSTR "20241018"
 
-#define MOTOR_ANGLE_DEG         28.5
+#define MOTOR_ANGLE_DEG         30
 #define BASE_Z_ROTATION_DEG     30
-
-// Carefully tuned magic rotation parameters for 149mm glass globe
-#define STEPS_PER_ROTATION_X         (2*4050)
-#define STEPS_PER_ROTATION_Y         (2*3945)
-#define STEPS_PER_ROTATION_Z         (2*4275)
-#define STEPS_PER_ROTATION_H         (2*4130)
-// avg = 8180
-// X = avg - 1.0%
-// Y = avg - 3.5%
-// Z = avg + 4.5%
 
 // Globe Diam : 160mm
 // Wheel Diam : 58mm
-// Motor steps: 200 * microstepping = 3200
+// Motor steps: 200 * microstepping = 200*16 = 3200
 // Steps per revolution: 3200 steps / 58mm * 160mm = 8827
 #define STEPS_PER_ROTATION         		(3200*160/58)
 
